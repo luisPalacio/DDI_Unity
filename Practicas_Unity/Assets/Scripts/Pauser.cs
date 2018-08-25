db1cc8a4bfefc62a92f888d28120b132bdb9df50
Luis Palacio Renteria <palacio.204@gmail.com>
2018-08-24 19:42:52 -0700

    Modificar Pause

--- Practicas_Unity/Assets/Scripts/Pauser.cs
+++ Practicas_Unity/Assets/Scripts/Pauser.cs
@@ -1,19 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Pauser : MonoBehaviour {
-	private bool paused = false;
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyUp(KeyCode.P))
-		{
-			paused = !paused;
-		}
+    private bool paused = false;
+    public GameObject pausePanel;
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            paused = !paused;
+            pausePanel.SetActive(paused);
+        }
 
-		if(paused)
-			Time.timeScale = 0;
-		else
-			Time.timeScale = 1;
-	}
+        if (paused)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+    }
+    public void continueGame()
+    {
+        Debug.Log("Continuar");
+        paused = false;
+        pausePanel.SetActive(paused);
+    }
+    public void ResetGame()
+    {
+        Debug.Log("Reiniciar Jugeo");
+        SceneManager.LoadScene(0);
+    }
 }
+
