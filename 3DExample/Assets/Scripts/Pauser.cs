48b9a60dcd8fa3cc94ec0edfd3f83fbe5e30887d
Luis Palacio Renteria <palacio.204@gmail.com>
2018-09-12 12:46:45 -0700

    stats

--- 3DExample/Assets/Scripts/Pauser.cs
+++ 3DExample/Assets/Scripts/Pauser.cs
@@ -1,21 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
-using UnityEngine.Audio;
 
 public class Pauser : MonoBehaviour
 {
     private bool paused = false;
     public GameObject pausePanel;
-    public GameObject pauseMusic;
 
+    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.P))
         {
-
             paused = !paused;
             pausePanel.SetActive(paused);
+           
         }
 
         if (paused)
@@ -23,21 +22,23 @@ public class Pauser : MonoBehaviour
         else
             Time.timeScale = 1;
     }
-    public void ResetGame()
-    {
-        Debug.Log("Reiniciar Jugeo");
-        SceneManager.LoadScene(0);
-    }
+
     public void ContinueGame()
     {
-        Debug.Log("Continuar");
+        Debug.Log("Continuar...");
         paused = false;
         pausePanel.SetActive(paused);
     }
-    public void MusicGame()
+
+    public void ResetGame()
     {
-        Debug.Log("Music");
-
-
+        Debug.Log("ResetGame...");
+        SceneManager.LoadScene("Level_Modified");
     }
+
+    public void MuteMusic()
+    {
+        Debug.Log("MuteMusic...");
+    }
+
 }
