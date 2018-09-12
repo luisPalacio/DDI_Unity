fc6b96c1ab18ee299803a818d6288f737de7e0d7
Luis Palacio Renteria <palacio.204@gmail.com>
2018-09-11 21:20:50 -0700

    Pauser En Rock

---
+++ 3DExample/Assets/Scripts/Pauser.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class Pauser : MonoBehaviour
+{
+    private bool paused = false;
+    public GameObject pausePanel;
+    public GameObject pauseMusic;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+
+            paused = !paused;
+            pausePanel.SetActive(paused);
+        }
+
+        if (paused)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+    }
+    public void ResetGame()
+    {
+        Debug.Log("Reiniciar Jugeo");
+        SceneManager.LoadScene(0);
+    }
+    public void ContinueGame()
+    {
+        Debug.Log("Continuar");
+        paused = false;
+        pausePanel.SetActive(paused);
+    }
+    public void MusicGame()
+    {
+        Debug.Log("Music");
+
+
+    }
+}
