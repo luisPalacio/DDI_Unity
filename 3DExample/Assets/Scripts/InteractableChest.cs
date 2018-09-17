0ae8c9d4bb0d0b8aa0e99ea4ffc7688678304b54
Luis Palacio Renteria <palacio.204@gmail.com>
2018-09-16 22:38:30 -0700

    Baul

---
+++ 3DExample/Assets/Scripts/InteractableChest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableChest : MonoBehaviour
+{
+    public Color color;
+    public GameObject inPanel;
+    private bool isPlayerIn = false;
+    
+
+    private void Start()
+    {
+        inPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        
+
+        if (Input.GetKeyDown(KeyCode.O) && isPlayerIn)
+        {
+            Open();
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inPanel.SetActive(true);
+            isPlayerIn = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inPanel.SetActive(false);
+            isPlayerIn = false;
+        }
+    }
+    public void Open ()
+    {
+            Destroy(this.gameObject);
+        }
+    
+}
