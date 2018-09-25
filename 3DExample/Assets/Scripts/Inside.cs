3b0ec010eef1bc85d54efd92cc67b6f91d227a6e
Luis Palacio Renteria <palacio.204@gmail.com>
2018-09-25 16:05:29 -0700

    car update

---
+++ 3DExample/Assets/Scripts/Inside.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class Inside : MonoBehaviour {
+    private bool isPlayerIn = false;
+    public GameObject car;
+    public GameObject persona;
+    CarController myScript;
+    CarUserControl myScript2;
+    public Camera Camera_Pers;
+    public Camera Camera_Carro;
+
+
+    // Use this for initialization
+    void Start () {
+        myScript = gameObject.GetComponent<CarController>();
+        myScript2 = gameObject.GetComponent<CarUserControl>();
+        persona.SetActive(true);
+        car.SetActive(true);
+        Camera_Carro.enabled = false;
+
+
+    }
+     //drag the gameobject which have that script you want to disable, in the inspector.
+ 
+     
+private void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.O) && isPlayerIn)
+        {
+            Open();
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+           
+            isPlayerIn = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerIn = false;
+        }
+    }
+    public void Open()
+    {
+        Camera_Pers.enabled = false;
+        myScript.enabled = !myScript.enabled;
+        myScript2.enabled = !myScript2.enabled;
+        persona.SetActive(false);
+        car.SetActive(true);
+        Camera_Carro.enabled = true;
+
+    }
+}
